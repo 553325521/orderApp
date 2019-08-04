0071ad774701e8aa8ce9b45c59c5740efe53f0ca
wangjinglong250 <1393936249@qq.com>
2019-08-04 20:36:58 +0800

    经营概况--筛选提交

--- pages/summary/summary.js
+++ pages/summary/summary.js
@@ -6,7 +6,6 @@ Page({
     shopArr: [],
     shopIndexArray:[],
     shopIndex: 0,
-    dateArr: ['今天', '昨天', '本周', '本月'],
     dateIndex: 0,
     memberNumber: 0,
     innerOrderNumber:0,
@@ -24,7 +23,14 @@ Page({
     wxPayOrderMoney: 0,
     aliPayOrderMoney: 0,
     posPayOrderMoney: 0,
-    cashPayOrderMoney: 0
+    cashPayOrderMoney: 0,
+    shaiXuanBtnIsActive:false, //筛选按钮是否被激活
+    selectShow: 0,
+    start_date: '2019-01-01',
+    end_date: '2019-01-01',
+    dateArr: ['今天', '昨天', '本周', '本月', '本年'],
+    dateIndex: 0,
+    select_id: -1
   },
   onLoad: function (options) {
   
@@ -33,7 +39,7 @@ Page({
   
   },
   onShow: function () {
-    this.loadShopRunData();
+    this.loadShopRunData("今天");
   },
   bindShopChange:function(e){
     this.setData({
@@ -48,6 +54,78 @@ Page({
       dateIndex: e.detail.value
     });
     this.loadShopRunData();
+  },
+  selectBtn: function (e) {
+    var id = e.currentTarget.dataset.id;
+    console.info(e);
+    var that = this;
+    if (id == 5) {
+      that.setData({
+        dateAreaIsShow: true,
+        takeView: 0
+      })
+    } else {
+      that.setData({
+        dateAreaIsShow: false,
+        takeView: 54
+      })
+    }
+    that.setData({
+      select_id: id
+    })
+  },
+  //关闭筛选
+  closeSelectArea: function () {
+    var that = this;
+    that.setData({
+      selectShow: 0,
+      shaiXuanBtnIsActive: false
+    })
+  },
+  cancelChoose: function () {
+    var that = this;
+    that.setData({
+      select_id: -1,
+      dateAreaIsShow: false,
+      takeView: 54
+    });
+  },
+  choose: function () {
+    var that = this;
+    var choosePay = "";
+    if (that.data.select_id == 5) {
+      choosePay = '自定义';
+    } else {
+      choosePay = that.data.dateArr[that.data.select_id];
+    }
+    this.loadShopRunData(choosePay);
+    this.closeSelectArea();
+    },
+  bindStartDateChange: function (e) {
+    this.setData({
+      start_date: e.detail.value
+    })
+  },
+  bindEndDateChange: function (e) {
+    this.setData({
+      end_date: e.detail.value
+    })
+  },
+  //点击筛选
+  openSelect: function () {
+    var that = this;
+    var isActive = that.data.shaiXuanBtnIsActive;
+    if (!isActive) {
+      that.setData({
+        selectShow: 100,
+        shaiXuanBtnIsActive:true
+      })
+    } else {
+      that.setData({
+        selectShow: 0,
+        shaiXuanBtnIsActive:false
+      })
+    }
   },
   //加载当前用户管理的商铺
   // loadUserShopArray:function(){
@@ -84,10 +162,8 @@ Page({
   //   })
   // },
 //根据选择店铺和时间加载经营概况
-loadShopRunData:function(){
+loadShopRunData:function(selectTime){
   var that = this;
-  //当前选择的时间
-  var selectTime = that.data.dateArr[that.data.dateIndex];
   //当前选择的店铺
   var shopId = app.globalData.shopid;
   wx.request({
@@ -95,7 +171,9 @@ loadShopRunData:function(){
     method: "post",
     data: {
       FK_SHOP:shopId,
-      selectTime:selectTime
+      selectTime:selectTime,
+      START_DATE:that.data.start_date,
+      END_DATE:that.data.end_date
     },
     header: {
       'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
