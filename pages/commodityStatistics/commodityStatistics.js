468400e230b1ae73e2794cbaa0b6961533e512ae
wangjinglong250 <1393936249@qq.com>
2019-08-05 16:04:39 +0800

    商品统计筛选提交

--- pages/commodityStatistics/commodityStatistics.js
+++ pages/commodityStatistics/commodityStatistics.js
@@ -6,11 +6,20 @@ Page({
     shopIndex:0,
     dateArr: ['今天', '昨天', '本周', '本月'],
     dateIndex: 0,
-    operatorArr: ['用户自点'],
+    operatorArr: ['用户自点','操作员1','操作员2'],
     operatorIndexArray:[0],
     operatorIndex: 0,
     productList:[],
-    isExistData:false
+    isExistData:false,
+    shaiXuanBtnIsActive:false,
+    selectShow: 0,
+    start_date: '2019-01-01',
+    end_date: '2019-01-01',
+    dateArr: ['今天', '昨天', '本周', '本月', '本年'],
+    dateIndex: 0,
+    select_id: -1,
+    operator_id:-1,
+    takeView:54
   },
   onLoad: function (options) {
   
@@ -20,6 +29,7 @@ Page({
   },
   onShow: function () {
     this.loadOperator();
+    this.loadGoodsData('今天');
   },
   //切换日期事件
   bindDateChange: function (e) {
@@ -27,6 +37,85 @@ Page({
       dateIndex: e.detail.value
     });
     this.loadGoodsData();
+  },
+  selectOperator:function(e){
+    var id = e.currentTarget.dataset.id;
+    var that = this;
+    that.setData({
+      operator_id: id
+    })
+  },
+  selectBtn: function (e) {
+    var id = e.currentTarget.dataset.id;
+    var that = this;
+    if (id == 5) {
+      that.setData({
+        dateAreaIsShow: true,
+        takeView: 0
+      })
+    } else {
+      that.setData({
+        dateAreaIsShow: false,
+        takeView: 54
+      })
+    }
+    that.setData({
+      select_id: id
+    })
+  },
+  //关闭筛选
+  closeSelectArea: function () {
+    var that = this;
+    that.setData({
+      selectShow: 0,
+      shaiXuanBtnIsActive: false
+    })
+  },
+  cancelChoose: function () {
+    var that = this;
+    that.setData({
+      select_id: -1,
+      operator_id:-1,
+      dateAreaIsShow: false,
+      takeView: 54
+    });
+  },
+  choose: function () {
+    var that = this;
+    var choosePay = "";
+    if (that.data.select_id == 5) {
+      choosePay = '自定义';
+    } else {
+      choosePay = that.data.dateArr[that.data.select_id];
+    }
+    // this.loadShopRunData(choosePay);
+    this.closeSelectArea();
+  },
+  bindStartDateChange: function (e) {
+    this.setData({
+      start_date: e.detail.value
+    })
+  },
+  bindEndDateChange: function (e) {
+    this.setData({
+      end_date: e.detail.value
+    })
+  },
+  //点击筛选
+  openSelect: function () {
+    var that = this;
+    var isActive = that.data.shaiXuanBtnIsActive;
+    if (!isActive) {
+      that.setData({
+        selectShow: 100,
+        shaiXuanBtnIsActive: true
+      })
+    } else {
+      that.setData({
+        selectShow: 0,
+        shaiXuanBtnIsActive: false
+      })
+    }
   },
   //切换操作员事件
   bindOperatorChange:function(e){
@@ -73,7 +162,7 @@ Page({
     })
   },
   //加载商品统计数据
-  loadGoodsData:function(){
+  loadGoodsData: function (selectTime){
     var that = this;
     app.sendRequest({
       url: "loadGoodsCountData",
@@ -81,7 +170,9 @@ Page({
       data: {
         FK_SHOP: app.globalData.shopid,
         FK_USER: that.data.operatorIndexArray[that.data.operatorIndex],
-        selectTime: that.data.dateArr[that.data.dateIndex],
+        selectTime: selectTime,
+        START_TIME:that.data.start_date,
+        END_TIME:that.data.end_date
       },
       success: function (res) {
         console.info(res.data);
